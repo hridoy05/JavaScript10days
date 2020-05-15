f7068e25cc4293ed318b00c0f971f136a53c12a6
Hridoy Banik <hridoy.banik71@gmail.com>
2020-05-15 23:47:33 +0600

    hacker rank 10 days problem done

---
+++ Day 0/Hello World.js
@@ -0,0 +1,9 @@
+
+function greeting(parameterVariable) {
+    // This line prints 'Hello, World!' to the console:
+    console.log('Hello, World!');
+    console.log(parameterVariable);
+
+    
+    
+}
---
+++ Day 7/Regular Expressions I.js
@@ -0,0 +1,8 @@
+function regexVar() {
+    
+    
+    var re = RegExp(/^([aeiou]).*\1$/);
+    
+    
+    return re;
+}
